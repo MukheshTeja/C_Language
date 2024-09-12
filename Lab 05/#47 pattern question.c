bc91ef03494cd4503f5ff461e55cdf8861567ec5
MukheshTeja <mukhesh2006teja@gmail.com>
2024-09-12 15:24:31 +0530

    Create #47 pattern question.c
    a
    ab
    abc
    abcd
    abcde

---
+++ Lab 05/#47 pattern question.c
@@ -0,0 +1,23 @@
+//47
+#include <stdio.h>
+int main()
+{
+    int j;
+    for (int i=1;i<=5;i++)
+    {
+        for (int j=1;j<=i;j++)
+        {
+            if (j==1)
+            printf("a");
+            if (j==2)
+            printf("b");
+            if (j==3)
+            printf("c");
+            if (j==4)
+            printf("d");
+            if (j==5)
+            printf("e");
+            
+        }printf("\n");
+    }
+}
