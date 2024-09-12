d1afe430e419e05d6b195dcb07d9ecaba3a1212f
MukheshTeja <mukhesh2006teja@gmail.com>
2024-09-12 15:20:19 +0530

    Create #50 pattern question.c
    a
    bb
    ccc
    dddd
    eeeee

---
+++ Lab 05/#50 pattern question.c
@@ -0,0 +1,23 @@
+//50
+#include <stdio.h>
+int main()
+{
+    int j;
+    for (int i=1;i<=5;i++)
+    {
+        for (int j=1;j<=i;j++)
+        {
+            if (i==1)
+            printf("a");
+            if (i==2)
+            printf("b");
+            if (i==3)
+            printf("c");
+            if (i==4)
+            printf("d");
+            if (i==5)
+            printf("e");
+            
+        }printf("\n");
+    }
+}
