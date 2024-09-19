51f0b2cb4e1dd7dde641d6becf5f2f0fdec8cd1b
MukheshTeja <mukhesh2006teja@gmail.com>
2024-09-19 12:50:45 +0530

    Create #36 odd_number till n.c

---
+++ Lab 3-4/#36 odd_number till n.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+    int n,a;
+    printf("enter the number of numbers you want in your series:");
+    scanf("%d",&n);
+    a=1;
+    for (int i=0;i<n;i++)
+    {
+        printf(">>%d",a);
+        a+=2;
+    }
+    
+    
+}
